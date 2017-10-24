601a227a6850d79630e5269b9c7ec9f264b69ab8
BP1\hemosq <Mikel.Herrero@bp.com>
2017-10-24 16:56:41 +0100

    Added changes modified logic

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # rhmap-params
-RHMAP module to retrieve fhparams of the running server
+This module adds a new endpoint to the running Cloud app. This new endpoint returns back the fh parameters needed to do a manual call from another environment.
+
+The params check returns a JSON response formatted as shown 
+below (using a single whitespace between key and values):
+
+```json
+{
+    "cuid":"<uuid>",
+    "cuidMap":null,
+    "destination":"web",
+    "sdk_version":"FH_PHONEGAP_SDK",
+    "appid":"<App ID>",
+    "appkey":"<App Key>",
+    "projectid":"<Project Id>",
+    "connectiontag":"<Connection Tag>",
+}
+
+##Usage
+The module should be initialised from your application.js 
+file as shown below. This will setup a new endpoint in your application called "/params", so ensure none of your endpoints are called params to avoid conflicts.
+
+This modules requires an API_KEY, this can be created at: Profile > Settings > API Key Management > Add New Key
+
+```javascript
+// Before var app = express()
+require('rhmap-params')(app,"<API_KEY>");
+```
--- route/params.js
+++ route/params.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var cors = require('cors');
 var request = require('request-promise');
+var xmlJs = require('xml-js');
+var uuid = require('uuid');
 
 function paramsRoute(apiKey) {
   var params = new express.Router();
@@ -8,9 +10,12 @@ function paramsRoute(apiKey) {
 
   // GET REST endpoint - query params may or may not be populated
   params.get('/', function(req, res) {
-    getConnections(apiKey).then(function(connection) {
-      res.json(connection);
-    }).catch(function(error) {
+    getConnections(apiKey)
+    .then(getFhConfig)
+    .then(function(fhParams) {
+      res.json(fhParams);
+    })
+    .catch(function(error) {
       res.json(error);
     });
   });
@@ -33,8 +38,8 @@ var getConnections = function(apiKey) {
           console.log('connections:', arrayConnections);
           for (var i = 0, len = arrayConnections.length; i < len; i++) {
             var connection = arrayConnections[i];
-            if (connection.environment == process.env.FH_ENV) {
-              return resolve(connection);
+            if (connection.environment == process.env.FH_ENV && connection.destination == "android") {
+              return resolve({"apiKey": apiKey, "connection": connection});
             }
           }
           reject({"msg":"No environment connection found"});
@@ -43,6 +48,67 @@ var getConnections = function(apiKey) {
           reject({"msg": "Error calling to connections endpoint"});
       });
   });
+};
+
+var getFhConfig = function(object) {
+  return new Promise(function (resolve, reject) {
+    var options = {
+      uri: 'https://'+ process.env.FH_MILLICORE + '/box/api/connections/' + object.connection.guid + '/sdk_config',
+      headers: {
+          'X-FH-AUTH-USER': object.apiKey
+      },
+      json: true // Automatically parses the JSON string in the response
+    };
+  
+    request(options)
+      .then(function (fhConfig) {
+        resolve(convertToFhParams(fhConfig));
+      })
+      .catch(function (err) {
+          reject({"msg": "Error calling to sdk_config endpoint"});
+      });
+  });
+};
+
+var convertToFhParams = function(fhConfig) {
+
+  var config = {
+    "host" : "",
+    "appid" : "",
+    "projectid" : "",
+    "appkey" : "",
+    "connectiontag" : "",
+  }
+
+  if (fhConfig.filename == "fhconfig.plist") {
+    var jsonConfig = xmlJs.xml2js(fhConfig.config, {compact:true, spaces:2});
+    var plist = jsonConfig.plist.dict;
+    for (var index = 0; index < plist.key.length; index++) {
+      var key = plist.key[index];
+      var text = plist.string[index];
+      config[key._text] = text._text;
+    }
+  } else if (fhConfig.filename == "fhconfig.json") {
+    config = JSON.parse(fhConfig.config);
+  } else if (fhConfig.filename == "fhconfig.properties") {
+    var properties = fhConfig.config.split("\n").slice(0,-1);
+    for (var index = 0; index < properties.length; index++) {
+      var property = properties[index].split(" = ");
+      config[property[0]] = property[1];
+    }
+  }
+
+  var fhParams = {
+    "cuid": uuid.v1().replace(new RegExp("-","g"),"").toUpperCase(),
+    "cuidMap": null,
+    "destination": "web",
+    "sdk_version": "FH_PHONEGAP_SDK",
+    "appid": config.appid,
+    "appkey": config.appkey,
+    "projectid": config.projectid,
+    "connectiontag": config.connectiontag
+  }
+  return fhParams;
 } 
 
 module.exports = paramsRoute;
